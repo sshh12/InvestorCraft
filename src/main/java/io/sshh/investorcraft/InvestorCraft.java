2b67456bfc30435fd77f13240e0fecd0e327fbbd
Shrivu Shankar <shrivu1122@gmail.com>
2019-05-23 00:57:37 -0500

    Added API

--- src/main/java/io/sshh/investorcraft/InvestorCraft.java
+++ src/main/java/io/sshh/investorcraft/InvestorCraft.java
@@ -1,5 +1,15 @@
 package io.sshh.investorcraft;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.milkbowl.vault.chat.Chat;
@@ -14,11 +24,12 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InvestorCraft extends JavaPlugin {
-    
+
     private static final Logger log = Logger.getLogger("Minecraft");
     private static Economy econ = null;
     private static Permission perms = null;
     private static Chat chat = null;
+    private static PriceAPI priceAPI = null;
 
     @Override
     public void onDisable() {
@@ -27,15 +38,16 @@ public class InvestorCraft extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if (!setupEconomy() ) {
+        if (!setupEconomy()) {
             log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
         setupPermissions();
         setupChat();
+        loadConfig();
     }
-    
+
     private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
@@ -47,7 +59,7 @@ public class InvestorCraft extends JavaPlugin {
         econ = rsp.getProvider();
         return econ != null;
     }
-    
+
     private boolean setupChat() {
         RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
         if (rsp != null) {
@@ -55,35 +67,41 @@ public class InvestorCraft extends JavaPlugin {
         }
         return chat != null;
     }
-    
+
     private boolean setupPermissions() {
         RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
         perms = rsp.getProvider();
         return perms != null;
     }
-    
+
+    public void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        priceAPI = new PriceAPI(getConfig().getString("investing.alphavantagekey"));
+        saveConfig();
+    }
+
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-        
-        if(!(sender instanceof Player)) {
+
+        if (!(sender instanceof Player)) {
             log.info("Only players are supported for this Example Plugin, but you should not do this!!!");
             return true;
         }
-        
+
         Player player = (Player) sender;
-        
-        if(command.getLabel().equals("test-economy")) {
+
+        if (command.getLabel().equals("test-economy")) {
             // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
-            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player.getName()))));
+            sender.sendMessage(String.format("You have %s %f", econ.format(econ.getBalance(player.getName())), priceAPI.getPrice("MSFT")));
             EconomyResponse r = econ.depositPlayer(player, 1.05);
-            if(r.transactionSuccess()) {
+            if (r.transactionSuccess()) {
                 sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
             } else {
                 sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
             }
             return true;
-        } else if(command.getLabel().equals("test-permission")) {
+        } else if (command.getLabel().equals("test-permission")) {
             // Lets test if user has the node "example.plugin.awesome" to determine if they are awesome or just suck
-            if(perms.has(player, "example.plugin.awesome")) {
+            if (perms.has(player, "example.plugin.awesome")) {
                 sender.sendMessage("You are awesome!");
             } else {
                 sender.sendMessage("You suck!");
@@ -92,19 +110,51 @@ public class InvestorCraft extends JavaPlugin {
         } else {
             return false;
         }
-        
+
     }
-    
+
     public static Economy getEconomy() {
         return econ;
     }
-    
+
     public static Permission getPermissions() {
         return perms;
     }
-    
+
     public static Chat getChat() {
         return chat;
     }
+
+}
+
+class PriceAPI {
     
+    private final static String BASE_URL = "https://www.alphavantage.co/";
+    private String key;
+
+    public PriceAPI(String apiKey) {
+        key = apiKey;
+    }
+
+    private JsonObject fetch(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection request = url.openConnection();
+            request.connect();
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject rootObj = root.getAsJsonObject();
+            return rootObj;
+        } catch (MalformedURLException ex) {
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    public double getPrice(String symbol) {
+        String url = String.format("%squery?function=GLOBAL_QUOTE&symbol=%s&apikey=%s", BASE_URL, symbol, key);
+        return fetch(url).getAsJsonObject("Global Quote").get("05. price").getAsDouble();
+    }
+
 }
